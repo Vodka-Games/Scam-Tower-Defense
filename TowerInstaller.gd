cd6953f3641416f76b2995acd43fc292b829a586
raspberry-pi <raspberry-pi@kakao.com>
2023-08-27 06:03:33 +0900

    Add decreasing Tower Item

--- TowerInstaller.gd
+++ TowerInstaller.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var is_picking = false
+var tower_name
 
 func pick_tower(pos, tower):
 	if is_picking:
@@ -15,9 +16,14 @@ func pick_tower(pos, tower):
 	
 func install_tower(obj):
 	self.is_picking = false
+	print(tower_name)
+	Global.install_tower(tower_name)
 
 func _on_ui_ingame_click_tower_item(idx):
 	var tower = Global.get_tower(idx)
+	if tower == null:
+		return 
+	self.tower_name = tower['name']
 	var tower_scene = Global.get_tower_scene(tower['name'])
 	
 	var pos = get_global_mouse_position()
